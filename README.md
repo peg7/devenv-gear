eb13e40fe5be279f7d7fe34d086c57c33ac0e140
peg7 <108526093+peg7@users.noreply.github.com>
2022-10-10 22:06:43 +0900

    Update devenv-setup

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # devenv-gear
+
+## Setup devenv
+
+```
+$ curl -sS https://raw.githubusercontent.com/peg7/devenv-gear/main/devenv-setup.sh | bash
+```
+
+ascii text は下記で作った。
+https://textkool.com/en/ascii-art-generator?hl=default&vl=default&font=Puffy&text=Setup
--- devenv-setup.sh
+++ devenv-setup.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+cat << EOF
+ ___           _                 
+(  _`\        ( )_               
+| (_(_)   __  | ,_) _   _  _ _   
+`\__ \  /'__`\| |  ( ) ( )( '_`\ 
+( )_) |(  ___/| |_ | (_) || (_) )
+`\____)`\____)`\__)`\___/'| ,__/'
+                          | |    
+                          (_)    
+EOF
+
 cd
 mkdir archive
 curl -o archive/devenv.dist.yml https://raw.githubusercontent.com/peg7/playbooks/main/devenv.dist.yml
